5913c055939f0b22502ab8c9c96a87998adb6dfc
DLBY <david.lobry96@gmail.com>
2022-12-07 11:39:27 +0100

    Feat (planet) : Add dynamic routing + common component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ import { NavMobile } from './NavMobile/NavMobile';
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center p-4 border-b border-gray-secondary">
+    <nav className="flex md:flex-col lg:flex-row justify-between items-center p-4 border-b border-gray-secondary">
       <div className="flex items-center">
         <span className="font-serif text-3xl uppercase tracking-tightest">The Planets</span>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -4,33 +4,41 @@ import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
 import App from './App';
 import './assets/styles/index.css';
 import { Mercury, Venus } from './pages/index';
+import { Planet } from './components/index';
 import { planetsData } from './data/data';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/mercury',
-    element: <Mercury />,
-  },
-  {
-    path: '/venus',
-    element: <Venus />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//   },
+//   {
+//     path: '/mercury',
+//     element: <Mercury />,
+//   },
+//   {
+//     path: '/venus',
+//     element: <Venus />,
+//   },
+// ]);
 
 // TODO
 // 1. create common components for each planet
 // 2. create dynamic routes for each planet
 
-// const router = createBrowserRouter(
-//   planetsData.map((planet) => ({
-//     path: `/${planet.name}`,
-//     element: <Planet />,
-//   }))
-// );
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+    },
+  ].concat(
+    planetsData.map((planet) => ({
+      path: `/${planet.name}`,
+      element: <Planet planetData={planet} />,
+    }))
+  )
+);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
